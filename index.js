f89a3524e7af545ecdbb22c368f1df0ffcc9f914
Gabriele <gblazyte8@gmail.com>
2023-01-23 18:44:40 +0200

    Create function that creates new grid

--- index.js
+++ index.js
@@ -2,28 +2,24 @@ const container = document.querySelector('.container');
 
 //console.log(container);
 
-// for (i = 0; i < 256; i++) {
-//     const square = document.createElement('div')
-//     square.className = 'square';
-//     container.appendChild(square);
-//     square.addEventListener('mouseover', e => e.target.classList.add('div-color'));
+function newGrid(num) {
+    let size = num * num;
+    for (i = 0; i < size; i++) {
+        const square = document.createElement('div')
+        square.className = 'square';
+        container.appendChild(square);
+        square.addEventListener('mouseover', e => e.target.classList.add('div-color'));
+    }
+};
 
-
-// }
 
 document.getElementById('btn').addEventListener('click', function () {
     let input = prompt('Choose the number of squares per side for the new grid');
     if (input > 100) {
         let input = prompt('Choose the number of squares per side between 1 and a 100 for the new grid');
     } else {
-        let num = input * input;
-        for (i = 0; i < num; i++) {
-            const square = document.createElement('div')
-            square.className = 'square';
-            container.appendChild(square);
-            square.addEventListener('mouseover', e => e.target.classList.add('div-color'));
-        }
+        newGrid(input);
     }
+});
 
-})
 
