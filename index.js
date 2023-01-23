6f4db6eae90c888f6c8c1d62014242460fab5251
Gabriele <gblazyte8@gmail.com>
2023-01-23 18:19:57 +0200

    Add a button to let user modify grid size

--- index.js
+++ index.js
@@ -2,15 +2,28 @@ const container = document.querySelector('.container');
 
 //console.log(container);
 
-for (i = 0; i < 256; i++) {
-    const square = document.createElement('div')
-    square.className = 'square';
-    container.appendChild(square);
-    square.addEventListener('mouseover', e => e.target.classList.add('div-color'));
+// for (i = 0; i < 256; i++) {
+//     const square = document.createElement('div')
+//     square.className = 'square';
+//     container.appendChild(square);
+//     square.addEventListener('mouseover', e => e.target.classList.add('div-color'));
 
 
-}
+// }
 
+document.getElementById('btn').addEventListener('click', function () {
+    let input = prompt('Choose the number of squares per side for the new grid');
+    if (input > 100) {
+        let input = prompt('Choose the number of squares per side between 1 and a 100 for the new grid');
+    } else {
+        let num = input * input;
+        for (i = 0; i < num; i++) {
+            const square = document.createElement('div')
+            square.className = 'square';
+            container.appendChild(square);
+            square.addEventListener('mouseover', e => e.target.classList.add('div-color'));
+        }
+    }
 
-
+})
 
