0a86ea73efb5d5bd0bb111670ff1a3e023f6ccb4
Gabriele <gblazyte8@gmail.com>
2023-01-30 19:06:55 +0200

    Change squares to be dynamic size

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const container = document.querySelector('.container');
 
-//console.log(container);
 
-function newGrid(num) {
-    let size = num * num;
-    for (i = 0; i < size; i++) {
-        const square = document.createElement('div')
+
+function newGrid(size) {
+    let squares = container.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    let num = size * size;
+    for (i = 0; i < num; i++) {
+        const square = document.createElement('div');
         square.className = 'square';
         container.appendChild(square);
         square.addEventListener('mouseover', e => e.target.classList.add('div-color'));
@@ -15,7 +19,7 @@ function newGrid(num) {
 
 document.getElementById('btn').addEventListener('click', function () {
     let input = prompt('Choose the number of squares per side for the new grid');
-    if (input > 100) {
+    if (input < 1 || input > 100) {
         let input = prompt('Choose the number of squares per side between 1 and a 100 for the new grid');
     } else {
         newGrid(input);
