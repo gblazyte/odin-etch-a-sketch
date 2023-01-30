bec13e2393849c8e844d2c18a2fefc4d2d565c12
Gabriele <gblazyte8@gmail.com>
2023-01-30 19:28:02 +0200

    Add styles to button and background

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const container = document.querySelector('.container');
 
-
+container.style.gridTemplateColumns = 'repeat(16, 1fr)';
+container.style.gridTemplateRows = 'repeat(16, 1fr)';
+for (i = 0; i < 256; i++) {
+    const square = document.createElement('div');
+    square.className = 'square';
+    container.appendChild(square);
+    square.addEventListener('mouseover', e => e.target.classList.add('div-color'));
+}
 
 function newGrid(size) {
     let squares = container.querySelectorAll('div');
